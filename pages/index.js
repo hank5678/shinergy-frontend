2237e9986f4d30e813c260ca8a324db8ff6418d6
Hank <hank5678tw@gmail.com>
2021-07-23 23:04:32 +0800

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,32 @@
+import Head from 'next/head'
+import Markdown from 'react-markdown'
+import axios from "axios"
+
+export default function Home({data}) {
+  return (
+    <div>
+      <Head>
+        <title>{data.seo.title}</title>
+        <meta name="description" content={data.seo.description} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <p>{data.title}</p>
+      
+      <Markdown>{data.bio}</Markdown>
+    </div>
+  )
+}
+
+export async function getStaticProps(context) {
+  const res = await axios({method: "get", url: "https://shinergy.herokuapp.com/home"})
+  const {data} = res
+  if (!data) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: { data }
+  }
+}
